8240e06b8487eb778c063b684590e173082313ea
nift4 <nift4@protonmail.com>
2024-10-06 18:05:01 +0200

    Upgrade kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
     val agpVersion = "8.7.0-beta02"
     id("com.android.application") version agpVersion apply false
     id("com.android.library") version agpVersion apply false
-    kotlin("android") version "2.0.20" apply false
+    kotlin("android") version "2.0.21-RC" apply false
 }
